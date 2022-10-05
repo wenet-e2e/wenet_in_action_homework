933dc2a4af8847ea50e0eea705cfa8a03b33d0e4
Binbin Zhang <binbzha@qq.com>
2022-10-05 23:48:31 +0800

    refine long wave

--- 10-long_form_asr/long_wave_transcript.py
+++ 10-long_form_asr/long_wave_transcript.py
@@ -5,7 +5,7 @@ from multiprocessing import Pool
 wenet.set_log_level(1)
 decoder = wenet.Decoder('./model')  # 初始化解码器
 
-vad = Vad()  # 初始化 VAD
+vad = Vad()  # 初始化 VAD, 详请参考 https://github.com/wiseman/py-webrtcvad
 segments = vad.process('./slice.wav')  # VAD 语音切分
 
 def decode(segment):
